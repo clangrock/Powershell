89f0289b8d3c4dc2aed2eb7047a0cd3a1dfaab1f
clangrock <Christian.LANGROCK@vinci-energies.net>
2021-11-03 20:18:11 +0100

    new script for GPO disable Windows Update Services
    GPO "Disable Windows Updates"

---
+++ Create-GPO-DisableWSU.ps1
@@ -0,0 +1,4 @@
+﻿$GPO = "Disable Windows Updates"
+New-GPO -Name $GPO | Set-GPRegistryValue -Key "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -ValueName "NoAutoUpdate" -Value 1 -Type "DWord" | Out-Null
+(Get-GPO -Name $GPO).User.Enabled=$False
+Get-GPO -Name $GPO
